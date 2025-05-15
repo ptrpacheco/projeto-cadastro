06e75b1f21d4aba1f27ef00efaadb030955c0758
Pietro <pieto.pacheco19@gmail.com>
2025-05-14 23:55:24 -0300

    programação família produtos

--- src/constants/CrudViewHeader.ts
+++ src/constants/CrudViewHeader.ts
@@ -1,53 +1,64 @@
 export const ClientsHeader = [
-   {
-      title: "NOME",
-      width: "w-44",
-   },
-   {
-      title: "CPF/CNPJ",
-      width: "w-38",
-   },
-   {
-      title: "TIPO",
-      width: "w-24",
-   },
-   {
-      title: "EMAIL",
-      width: "w-44",
-   },
-   {
-      title: "TELEFONE",
-      width: "w-38",
-   },
-   {
-      title: "ENDEREÇO",
-      width: "w-44",
-   },
-]
+  {
+    title: "NOME",
+    width: "w-44",
+  },
+  {
+    title: "CPF/CNPJ",
+    width: "w-38",
+  },
+  {
+    title: "TIPO",
+    width: "w-24",
+  },
+  {
+    title: "EMAIL",
+    width: "w-44",
+  },
+  {
+    title: "TELEFONE",
+    width: "w-38",
+  },
+  {
+    title: "ENDEREÇO",
+    width: "w-44",
+  },
+];
 
 export const SuppliersHeader = [
-   {
-      title: "NOME",
-      width: "w-44",
-   },
-   {
-      title: "CPF/CNPJ",
-      width: "w-38",
-   },
-   {
-      title: "TIPO",
-      width: "w-24",
-   },
-   {
-      title: "EMAIL",
-      width: "w-44",
-   },
-   {
-      title: "TELEFONE",
-      width: "w-38",
-   },
-   {
-      title: "ENDEREÇO",
-      width: "w-44",
-   },
-]
+  {
+    title: "NOME",
+    width: "w-44",
+  },
+  {
+    title: "CPF/CNPJ",
+    width: "w-38",
+  },
+  {
+    title: "TIPO",
+    width: "w-24",
+  },
+  {
+    title: "EMAIL",
+    width: "w-44",
+  },
+  {
+    title: "TELEFONE",
+    width: "w-38",
+  },
+  {
+    title: "ENDEREÇO",
+    width: "w-44",
+  },
+];
+
+export const ProductsFamiliesHeader = [
+  {
+    title: "CÓDIGO",
+    width: "w-24",
+  },
+  {
+    title: "FAMÍLIA",
+    width: "w-44",
+  },
+];
